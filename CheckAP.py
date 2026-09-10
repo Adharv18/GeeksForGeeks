7cc542a734987342435f2e391dccdb2b8da31f35
Adharv18 <adharvmakthedhar10@gmail.com>
2026-09-10 22:31:05 +0530

    Create CheckAP.py

---
+++ CheckAP.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkIsAP(self, arr):
+        arr.sort()
+        diff = arr[1]-arr[0]
+        for i in range(1,len(arr)):
+            if arr[i]-arr[i-1] != diff:
+                return False
+                        
+        return True
